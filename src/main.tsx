3470eb08ca42215ebbc5b3fc3777682544df642e
oohajo <99666208+oohajo@users.noreply.github.com>
2024-04-03 14:52:09 +0200

    Addded interfaces

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+interface Experience {
+  year: number;
+  description: string;
+}
+
+interface CVData {
+  photo: string;
+  name: string;
+  lastName: string;
+  position: string;
+  //tablica obiektow zgodnych z intefrace Experience
+  experience: Experience[];
+  //tablica ciagow znakow
+  education: string[];
+}
+
+//zanim cokolwiek wyrenderujemy tworzymy obiekt. ktory bedzie implementowac przygotowany interface
+const cvData: CVData = {
+  photo: "https://placehold.co/400x400",
+  name: "Diana",
+  lastName: "K.",
+  position: "Doctor of Sciences in Physics",
+  experience: [
+    {year: 2024, description: "Lorem ipsum"},
+    {year: 2020, description: "Dolor sit amet"},
+  ],
+  education: ["Sobotka", "noc krotka", "gwiazdy sie pala"]
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
